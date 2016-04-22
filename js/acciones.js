865af26be2425a3b5b30053b2de9895684751c08
Lanix 1 <Lanix 1>
2016-04-22 12:52:03 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-$(document).ready(function) (e){
+$(document).ready(function (e){
 	document.addEventListener("deviceready",function(){
 		$('#disp table td').eq(3).text(device.model);
 		$('#disp table td').eq(5).text(device.cordova);
